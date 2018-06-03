c6a3770011b8e2dd6e1ffec7d47f7931e23ad127
Francois Karman <francois.karman@kneip.com>
2018-06-03 20:43:25 +0200

    Integrate unit test project

---
+++ test/Kneip.AspNetCore.Mvc.Versioning.Tests/IServiceCollectionExtensionsTest.cs
@@ -0,0 +1,22 @@
+// Copyright (c) KNEIP Communication S.A.. All rights reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kneip.AspNetCore.Mvc.Versioning
+{
+    /// <summary>
+    /// Tests the <see cref="IServiceCollectionExtensions"/> class.
+    /// </summary>
+    [TestClass]
+    public class IServiceCollectionExtensionsTest
+    {
+        /// <summary>
+        /// Tests the <see cref="IServiceCollectionExtensions.AddConfiguredApiVersioning"/> method.
+        /// </summary>
+        [TestMethod]
+        public void AddConfiguredApiVersioning()
+        {
+        }
+    }
+}
