3d42384b7718d79b659d6378bfa122fadedc6477
Francois Karman <francois.karman@kneip.com>
2018-06-03 10:39:26 +0200

    implementation of the helpers

---
+++ src/Kneip.AspNetCore.Mvc.Versioning/IApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) KNEIP Communication S.A.. All rights reserved.
+
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extensions methods to configure api versioning and swagger.
+    /// </summary>
+    public static class IApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Activates swagger ui with support of the api versioning.
+        /// </summary>
+        /// <param name="app">The current application builder.</param>
+        public static void UseConfiguredApiVersioning(this IApplicationBuilder app)
+        {
+            // The api version descriptor
+            var provider = app.ApplicationServices.GetRequiredService<IApiVersionDescriptionProvider>();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    // Swagger available at the entry point of the application
+                    options.RoutePrefix = string.Empty;
+
+                    // Display versions in swagger documentation
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant());
+                    }
+                });
+        }
+    }
+}
---
+++ src/Kneip.AspNetCore.Mvc.Versioning/IServiceCollectionExtensions.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) KNEIP Communication S.A.. All rights reserved.
+
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.IO;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions methods to configure api versioning and swagger.
+    /// </summary>
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Initializes rest api versioning and the associated swagger configuration.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="title">The title of the api - used for the swagger documentation.</param>
+        /// <param name="setupAction">Extra setup Action for swagger.</param>
+        public static void AddConfiguredApiVersioning(this IServiceCollection services, string title, Action<SwaggerGenOptions> setupAction = null)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            else if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            // Activate api versioning
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+            });
+
+            // Activate Api Explorer - with versioning support
+            services.AddMvcCore()
+                .AddApiExplorer()
+                .AddVersionedApiExplorer(o =>
+                {
+                    o.GroupNameFormat = "'v'VVV";
+                    o.SubstituteApiVersionInUrl = true;
+                });
+
+            // Activate swagger json file generation
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // Integrate all versions into the swagger files
+                    var provider = services.BuildServiceProvider()
+                                           .GetRequiredService<IApiVersionDescriptionProvider>();
+
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        Info info = new Info()
+                        {
+                            Title = $"{title} {description.ApiVersion}",
+                            Version = description.ApiVersion.ToString()
+                        };
+
+                        options.SwaggerDoc(description.GroupName, info);
+                    }
+
+                    // Integrate xml docs
+                    foreach (var file in Directory.GetFiles(AppContext.BaseDirectory, "*.xml"))
+                    {
+                        options.IncludeXmlComments(file);
+                    }
+
+                    // Extra configuration steps, if any
+                    setupAction?.Invoke(options);
+                });
+        }
+    }
+}
